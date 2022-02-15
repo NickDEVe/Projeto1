d44dda3a73dda670d26a2304d2146b644c69e8bf
Nicolas Isamel <nicolasismael123@gmail.com>
2022-02-14 23:37:25 -0300

    initial commit

---
+++ javaScript.js
@@ -0,0 +1,28 @@
+let brownie= document.getElementById("brownie-img");
+
+function mouseFora(){
+
+    brownie.style.height= "200px";
+    brownie.style.width="200px";
+    brownie.style.borderColor="black";
+   
+}
+function mouseDentro(){
+    brownie.style.borderColor="red";
+    brownie.style.height= "220px";
+    brownie.style.width="220px";
+  
+
+
+}
+function escolheu(){
+    let siteEscolha= document.getElementById("pageBrownie");
+    
+
+}
+
+function calcular(){
+    let porçoes= document.getElementById("qtdCalcular");
+    
+    
+}
