82541a94ec750bb497a1660cc882924486447462
Nicolas Isamel <nicolasismael123@gmail.com>
2022-02-22 21:29:33 -0300

    adicionando novas imagens para a pagina inicial

--- javaScript.js
+++ javaScript.js
@@ -1,28 +1,71 @@
-let brownie= document.getElementById("brownie-img");
+let sobremessa1 = document.getElementById("brownie");
+let sobremessa2 = document.getElementById("mousse");
+let sobremessa3 = document.getElementById("tortaMaça");
+let sobremessa4 = document.getElementById("cookies");
+let porçoes= document.getElementById("qtdCalcular");
+
+// images efects
+
 
 function mouseFora(){
 
-    brownie.style.height= "200px";
-    brownie.style.width="200px";
-    brownie.style.borderColor="black";
+    sobremessa1.style.height= "200px";
+    sobremessa1.style.width="200px";
+    sobremessa1.style.borderColor="black";
+    
+}
+
+function mouseFora1(){
+   
+    sobremessa2.style.height= "200px";
+    sobremessa2.style.width="200px";
+    sobremessa2.style.borderColor="black";
+   
+
+}
+
+function mouseFora2(){
+    sobremessa3.style.height= "200px";
+    sobremessa3.style.width="200px";
+    sobremessa3.style.borderColor="black";
+   
+}
+
+function mouseFora3(){
+    sobremessa4.style.height= "200px";
+    sobremessa4.style.width="200px";
+    sobremessa4.style.borderColor="black";
    
 }
 function mouseDentro(){
-    brownie.style.borderColor="red";
-    brownie.style.height= "220px";
-    brownie.style.width="220px";
-  
-
-
+    sobremessa1.style.borderColor="red";
+    sobremessa1.style.height= "220px";
+    sobremessa1.style.width="220px";
 }
+
+function mouseDentro1(){
+    sobremessa2.style.borderColor="red";
+    sobremessa2.style.height= "220px";
+    sobremessa2.style.width="220px";
+}
+
+function mouseDentro2(){
+    sobremessa3.style.borderColor="red";
+    sobremessa3.style.height= "220px";
+    sobremessa3.style.width="220px";
+}
+
+function mouseDentro3(){
+    sobremessa4.style.borderColor="red";
+    sobremessa4.style.height= "220px";
+    sobremessa4.style.width="220px";
+}
+
+// go to page of choice
+
 function escolheu(){
     let siteEscolha= document.getElementById("pageBrownie");
     
 
 }
 
-function calcular(){
-    let porçoes= document.getElementById("qtdCalcular");
-    
-    
-}
